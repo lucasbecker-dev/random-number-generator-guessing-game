7adf25b95f624298f357bc337d751dfb81805e2e
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-11 12:12:27 -0400

    ran intellij's built-in formatter

--- com/coderscampus/assignment2/Main.java
+++ com/coderscampus/assignment2/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rng = new Random();
-        int secretNumber = rng.nextInt(1,101);
+        int secretNumber = rng.nextInt(1, 101);
         int remainingGuesses = 5;
         int playerGuess;
         boolean playerWon = false;
